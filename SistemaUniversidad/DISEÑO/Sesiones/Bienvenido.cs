9a47bee22fb084874f0447a5469a599fbaf32b08
alexu503 <us21003@ues.edu.sv>
2022-07-03 13:58:16 -0600

    Rearranging brackets xd

--- SistemaUniversidad/DISEÑO/Sesiones/Bienvenido.cs
+++ SistemaUniversidad/DISEÑO/Sesiones/Bienvenido.cs
@@ -11,12 +11,9 @@ using System.Data.SQLite;
 using System.Runtime.InteropServices;
 using SistemaUniversidad.LOGICA.DATABASE;
 
-namespace SistemaUniversidad.DISEÑO.Sesiones
-{
-    public partial class Bienvenido : Form
-    {
-        public Bienvenido()
-        {
+namespace SistemaUniversidad.DISEÑO.Sesiones {
+    public partial class Bienvenido : Form {
+        public Bienvenido() {
             InitializeComponent();
         }
 
@@ -25,29 +22,26 @@ namespace SistemaUniversidad.DISEÑO.Sesiones
         private extern static void ReleaseCapture();
         [DllImport("user32.DLL", EntryPoint = "SendMessage")]
         private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
-        private void pnlContBienve_MouseDown(object sender, MouseEventArgs e)
-        {
+        private void pnlContBienve_MouseDown(object sender, MouseEventArgs e) {
             ReleaseCapture();
             SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
-        private void lblBienvenido_MouseDown(object sender, MouseEventArgs e)
-        {
+        private void lblBienvenido_MouseDown(object sender, MouseEventArgs e) {
             ReleaseCapture();
             SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
-        private void lblLineaDivisora_MouseDown(object sender, MouseEventArgs e)
-        {
+        private void lblLineaDivisora_MouseDown(object sender, MouseEventArgs e) {
             ReleaseCapture();
             SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
         #endregion
-        private void btnIniciarSesion_Click(object sender, EventArgs e){
+        private void btnIniciarSesion_Click(object sender, EventArgs e) {
             Login.Login formLogin = new Login.Login();//Crea un objeto del formulario login
             this.Hide();//Oculta éste formulario
             formLogin.Show();//Se muestra el formulario de Login
         }
 
-        private void btnCerrar_Click(object sender, EventArgs e){
+        private void btnCerrar_Click(object sender, EventArgs e) {
             Application.Exit();
         }
 
