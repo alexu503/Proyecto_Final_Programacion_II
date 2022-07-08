05f69555695ee13cc2089efe5440d1a5771bbd33
alexu503 <us21003@ues.edu.sv>
2022-07-08 15:51:02 -0600

    Added 'Eliminar horarios' form

---
+++ SistemaUniversidad/DISEÑO/Administrador/Horarios/EliminarHorario.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace SistemaUniversidad.DISEÑO.Administrador.Horarios {
+    partial class EliminarHorario {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EliminarHorario));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblSelectDay = new System.Windows.Forms.Label();
+            this.lblSubjectList = new System.Windows.Forms.Label();
+            this.cmbSelectDay = new System.Windows.Forms.ComboBox();
+            this.cmbSelectSubject = new System.Windows.Forms.ComboBox();
+            this.cmbGetCareerList = new System.Windows.Forms.ComboBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.btnReturnToMainForm = new System.Windows.Forms.Button();
+            this.imgLogo = new System.Windows.Forms.PictureBox();
+            this.dgvViewSchedules = new System.Windows.Forms.DataGridView();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnCancell = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvViewSchedules)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblSelectDay);
+            this.groupBox1.Controls.Add(this.lblSubjectList);
+            this.groupBox1.Controls.Add(this.cmbSelectDay);
+            this.groupBox1.Controls.Add(this.cmbSelectSubject);
+            this.groupBox1.Controls.Add(this.cmbGetCareerList);
+            this.groupBox1.Controls.Add(this.lblNombre);
+            this.groupBox1.Location = new System.Drawing.Point(152, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(790, 217);
+            this.groupBox1.TabIndex = 33;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblSelectDay
+            // 
+            this.lblSelectDay.AutoSize = true;
+            this.lblSelectDay.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSelectDay.Location = new System.Drawing.Point(49, 170);
+            this.lblSelectDay.Name = "lblSelectDay";
+            this.lblSelectDay.Size = new System.Drawing.Size(133, 21);
+            this.lblSelectDay.TabIndex = 29;
+            this.lblSelectDay.Text = "Seleccionar día:";
+            // 
+            // lblSubjectList
+            // 
+            this.lblSubjectList.AutoSize = true;
+            this.lblSubjectList.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubjectList.Location = new System.Drawing.Point(49, 98);
+            this.lblSubjectList.Name = "lblSubjectList";
+            this.lblSubjectList.Size = new System.Drawing.Size(170, 21);
+            this.lblSubjectList.TabIndex = 30;
+            this.lblSubjectList.Text = "Seleccionar materia:";
+            // 
+            // cmbSelectDay
+            // 
+            this.cmbSelectDay.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbSelectDay.FormattingEnabled = true;
+            this.cmbSelectDay.Items.AddRange(new object[] {
+            "Lunes",
+            "Martes",
+            "Miércoles",
+            "Jueves",
+            "Viernes",
+            "Sábado"});
+            this.cmbSelectDay.Location = new System.Drawing.Point(531, 162);
+            this.cmbSelectDay.Name = "cmbSelectDay";
+            this.cmbSelectDay.Size = new System.Drawing.Size(215, 29);
+            this.cmbSelectDay.TabIndex = 24;
+            // 
+            // cmbSelectSubject
+            // 
+            this.cmbSelectSubject.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbSelectSubject.FormattingEnabled = true;
+            this.cmbSelectSubject.Location = new System.Drawing.Point(531, 90);
+            this.cmbSelectSubject.Name = "cmbSelectSubject";
+            this.cmbSelectSubject.Size = new System.Drawing.Size(215, 29);
+            this.cmbSelectSubject.TabIndex = 24;
+            // 
+            // cmbGetCareerList
+            // 
+            this.cmbGetCareerList.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbGetCareerList.FormattingEnabled = true;
+            this.cmbGetCareerList.Location = new System.Drawing.Point(531, 17);
+            this.cmbGetCareerList.Name = "cmbGetCareerList";
+            this.cmbGetCareerList.Size = new System.Drawing.Size(215, 29);
+            this.cmbGetCareerList.TabIndex = 24;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(49, 30);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(165, 21);
+            this.lblNombre.TabIndex = 23;
+            this.lblNombre.Text = "Seleccionar carrera:";
+            // 
+            // btnReturnToMainForm
+            // 
+            this.btnReturnToMainForm.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReturnToMainForm.Location = new System.Drawing.Point(1011, 12);
+            this.btnReturnToMainForm.Name = "btnReturnToMainForm";
+            this.btnReturnToMainForm.Size = new System.Drawing.Size(48, 35);
+            this.btnReturnToMainForm.TabIndex = 31;
+            this.btnReturnToMainForm.Text = "X";
+            this.btnReturnToMainForm.UseVisualStyleBackColor = true;
+            this.btnReturnToMainForm.Click += new System.EventHandler(this.btnReturnToMainForm_Click);
+            // 
+            // imgLogo
+            // 
+            this.imgLogo.Image = ((System.Drawing.Image)(resources.GetObject("imgLogo.Image")));
+            this.imgLogo.Location = new System.Drawing.Point(12, 12);
+            this.imgLogo.Name = "imgLogo";
+            this.imgLogo.Size = new System.Drawing.Size(125, 90);
+            this.imgLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgLogo.TabIndex = 32;
+            this.imgLogo.TabStop = false;
+            // 
+            // dgvViewSchedules
+            // 
+            this.dgvViewSchedules.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvViewSchedules.GridColor = System.Drawing.SystemColors.Control;
+            this.dgvViewSchedules.Location = new System.Drawing.Point(152, 283);
+            this.dgvViewSchedules.Name = "dgvViewSchedules";
+            this.dgvViewSchedules.Size = new System.Drawing.Size(790, 295);
+            this.dgvViewSchedules.TabIndex = 34;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(341, 621);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(98, 37);
+            this.btnAgregar.TabIndex = 37;
+            this.btnAgregar.Text = "Eliminar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancell
+            // 
+            this.btnCancell.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancell.Location = new System.Drawing.Point(640, 621);
+            this.btnCancell.Name = "btnCancell";
+            this.btnCancell.Size = new System.Drawing.Size(98, 37);
+            this.btnCancell.TabIndex = 36;
+            this.btnCancell.Text = "Cancelar";
+            this.btnCancell.UseVisualStyleBackColor = true;
+            // 
+            // EliminarHorario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1071, 686);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnCancell);
+            this.Controls.Add(this.dgvViewSchedules);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.imgLogo);
+            this.Controls.Add(this.btnReturnToMainForm);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "EliminarHorario";
+            this.Text = "EliminarHorario";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvViewSchedules)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblSelectDay;
+        private System.Windows.Forms.Label lblSubjectList;
+        private System.Windows.Forms.ComboBox cmbSelectDay;
+        private System.Windows.Forms.ComboBox cmbSelectSubject;
+        private System.Windows.Forms.ComboBox cmbGetCareerList;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.PictureBox imgLogo;
+        private System.Windows.Forms.Button btnReturnToMainForm;
+        private System.Windows.Forms.DataGridView dgvViewSchedules;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnCancell;
+    }
+}
